18c6d65be2a355cf433951f02f210aee2a86591b
ericcarvalhoprado-byte <eric.carvalho.prado@escola.pr.gov.br>
2026-09-16 10:49:00 -0300

    Add files via upload

---
+++ datahora.js
@@ -0,0 +1,12 @@
+function datahora(){
+  const agora = new Date();
+  const hora = agora.toLocaleDateString('pt-br');
+
+  const relogios = document.querySelectorAll('.relogio-post');
+
+
+
+
+
+
+}
